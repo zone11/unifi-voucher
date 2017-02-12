2a5a0309079f0d96b9199410d7ec2abd3e510a9d
Christian Egger <zone11@mac.com>
2017-02-12 16:06:59 +0100

    Config

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+// UniFi settings
+$cfgUnifiSiteid = 'default';
+$cfgUnifiUsername = 'admin';
+$cfgUnifiPassword = 'admin';
+$cfgUnifiBaseurl = 'https://1.2.3.4:8443';
+
+// Voucher
+$cfgUnifiVoucherNote = "unifi-voucher-php: ".date("Y.m.d");
+$cfgUnifiVoucherDuration = '1440'; // 1 day
+
+// Printing Voucher
+$cfgPrintStrWifiName = 'My WIFI SSID';
+$cfgPrintStrWifiTitle = 'Internet - Free as beer!';
+$cfgPrintLogoFile = 'resources/logo.png';
+
+// Printer
+$cfgPrinterConnection = "net";
+$cfgPrinterNetIP = "1.2.3.4";
+$cfgPrinterNetPort = 9100;
+?>
--- voucher.php
+++ voucher.php
@@ -8,34 +8,27 @@ require __DIR__ . '/vendor/autoload.php';
 // Unifi Class from UniFi API Browser
 require("./unifi/class.unifi.php");
 
+// Config
+require("./config.php");
+
 // Printer
 use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
 use Mike42\Escpos\EscposImage;
 use Mike42\Escpos\Printer;
 
-// Configs
-$unifi_siteid = 'default';
-$unifi_username = 'admin';
-$unifi_password = 'password';
-$unifi_baseurl = 'https://1.2.3.4:8443';
-$unifi_wifi_name = 'My WiFi';
-$unifi_wifi_title = 'Free as beer!';
-$unifi_voucher_note = "POS Voucher using API: ".date("Y.m.d H:i:s");
-$unifi_voucher_duration = '1440'; // 1 day
-
 // Prepare Printer and Logo
-$connector = new NetworkPrintConnector("1.2.3.4",9100);
-$printer = new Printer($connector);
-$logo = EscposImage::load("resources/logo.png", false);
+$printerConnector = new NetworkPrintConnector($cfgPrinterNetIP, $cfgPrinterNetPort);
+$printer = new Printer($printerConnector);
+$logo = EscposImage::load($cfgPrintLogoFile, false);
 
 // Connect!
-$unifi = new unifiapi($unifi_username, $unifi_password, $unifi_baseurl, $unifi_siteid);
+$unifi = new unifiapi($cfgUnifiUsername, $cfgUnifiPassword, $cfgUnifiBaseurl, $cfgUnifiSiteid);
 $login = $unifi->login();
 if ($login == 1) {
 	echo("Login: OK!\n");
 
 	//Create Voucher
-	$voucher = $unifi->create_voucher($unifi_voucher_time, 1,0,$unifi_voucher_note);
+	$voucher = $unifi->create_voucher($cfgUnifiVoucherDuration,1,0,$cfgUnifiVoucherNote);
 	
 	// We got a voucher as array item 0
 	if(sizeof($voucher) > 0) {
@@ -52,10 +45,10 @@ if ($login == 1) {
 		
 		// Name and title
 		$printer -> selectPrintMode(Printer::MODE_DOUBLE_WIDTH);
-		$printer -> text($unifi_wifi_name);
+		$printer -> text($cfgPrintStrWifiName);
 		$printer -> selectPrintMode();
 		$printer -> feed();
-		$printer -> text($unifi_wifi_title);
+		$printer -> text($cfgPrintStrWifiTitle);
 		$printer -> feed(2);
 		
 		// Voucher code
